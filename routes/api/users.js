30e6516e4fe6890fc7f3584f148d75a3821c3bff
Lilu <KHussell@gmail.com>
2020-07-23 16:20:52 -0400

    db set up

---
+++ routes/api/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+
+
+const User = require('../../models/Users');
+
+router.get('/', (req, res) => {
+    res.send('hey');
+})
+
+
+
+module.exports = router;
+
+
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db.js')
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+//app.use('/api/auth', require('./routes/api/auth'));
+//app.use('/api/profile', require('./routes/api/profile'));
+
+const PORT = process.env.PORT || 4500;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
